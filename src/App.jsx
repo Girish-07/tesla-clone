085c108585833f8796296488fefba797e84bafa7
Girish S <girishssiddanagoudar@gmail.com>
2025-03-19 11:05:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,226 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+        <div className="top1">
+            <p className="firtxt">Inventory Model Y available Now</p> 
+            <p className="sectxt"> Explore Reduced pricing</p>
+            <div className="btn">
+                <a href="#" id="btntxt"> View Inventory</a>
+            </div>
+        </div>
+        <div className="navbar">
+            <div className="logo"></div>
+            <div className="tabs">
+             <a href="#">Vehicles</a>
+             <a href="#">Energy</a>
+             <a href="#">Charging</a>
+             <a href="#">Discover</a>
+             <a href="#">Shop</a>
+            </div>
+            <div className="icons">
+             <a href="#"><i className="fa-regular fa-circle-question"></i></a>
+             <a href="#"><i className="fa-solid fa-globe"></i></a>
+             <a href="#"><i className="fa-solid fa-circle-user"></i></a>
+            </div>
+
+        </div>
+        
+    </header>
+
+    <div className="main1">
+        <div className="heading1">
+            <h1 className="heading1txt">Model 3</h1>
+        </div>
+        
+        <div className="heading2"> 
+            <a href="#" className="heading2txt">0% APR: Limited-Time Only</a>
+        </div>
+       <div className="buttons">
+        <div className="orderbtn">
+            <a href="#" className="ordertxt">Order Now</a>
+        </div>
+        <div className="learnbtn">
+            <a href="#" className="learntxt">Learn More</a>
+        </div>
+       </div>
+
+
+
+    </div>
+    <div className="main2">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Model 3</h2>
+        </div>
+        <div className="headingsec2">
+            <a href="#" className="headingsec2txt">$299/mo Leasing</a>
+        </div>
+        <div className="buttons2">
+            <div className="orderbtn2">
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">View Inventory</a>
+            </div>
+            </div>
+        </div>    
+
+
+
+    <div className="main3">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">New Model Y</h2>
+        </div>
+        <div className="headingsec2">
+            <a href="#" className="headingsec2txt">$7,500 Federal Tax Credit at Purchase</a>
+        </div>
+        <div className="buttons2">
+            <div className="orderbtn2">
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">Learn More</a>
+            </div>
+            </div>
+        </div>   
+
+
+    <div className="main4">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Model X</h2>
+        </div>
+        <div className="headingsec2">
+            <a href="#" className="headingsec2txt">Free Supercharging Included</a>
+        </div>
+        <div className="buttons2">
+            <div className="orderbtn2">
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">Learn More</a>
+            </div>
+        </div>
+    </div> 
+
+
+
+    <div className="main5">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Model S</h2>
+        </div>
+        <div className="headingsec2">
+            <a href="#" className="headingsec2txt">Free Supercharging Included</a>
+        </div>
+        <div className="buttons2">
+            <div className="orderbtn2">
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">Learn More</a>
+            </div>
+            </div>
+        </div> 
+    
+
+
+    <div className="main6">
+        <div className="headingsec1" id="cyber1">
+            <h2 className="headingsec1txt" id="cyber1txt">Cybertruck</h2>
+        </div>
+        <div className="headingsec2" id="cyber2">
+            <a href="#" className="headingsec2txt" id="cyber2txt">1.99% APR Ending March 31</a>
+        </div>
+        <div className="rating">
+            <i className="fa-solid fa-star star" ></i>
+            <i className="fa-solid fa-star star" ></i>
+            <i className="fa-solid fa-star star" ></i>
+            <i className="fa-solid fa-star star" ></i>
+            <i className="fa-solid fa-star star" ></i>
+                <p className="ratingtxt">Overall NHTSA Safety Rating</p>
+            
+
+        </div>
+        <div className="buttons2" id="cyberbuttons">
+            <div className="orderbtn2" id="cyberbtn1">
+                <a href="#" className="ordertxt2" id="cyberbtn1txt" >Order Now</a>
+            </div>
+            <div className="learnbtn2" id="cyberbtn2">
+                <a href="#" className="learntxt2" id="cyberbtn2txt">Learn More</a>
+            </div>
+        </div> 
+    </div>
+
+    <div className="main7">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Solar Panels</h2>
+        </div>
+        <div className="headingsec2">
+            <a href="#" className="headingsec2txt">Schedule a Virtual Consultation</a>
+        </div>
+        <div className="buttons2" id="solarbtn">
+            <div className="orderbtn2" >
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">Learn More</a>
+            </div>
+            </div>
+        </div> 
+    
+
+    <div className="main8">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Solar Roof</h2>
+        </div>
+        <div className="headingsec2">
+            <a href="#" className="headingsec2txt">Schedule a Virtual Consultation</a>
+        </div>
+        <div className="buttons2" id="roofbtn">
+            <div className="orderbtn2">
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">Learn More</a>
+            </div>
+            </div>
+        </div> 
+    
+    
+
+    <div className="main9">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Powerwall </h2>
+        </div>
+        
+        <div className="buttons2" id="pwbtn">
+            <div className="orderbtn2" >
+                <a href="#" className="ordertxt2" >Order Now</a>
+            </div>
+            <div className="learnbtn2">
+                <a href="#" className="learntxt2">Learn More</a>
+            </div>
+            </div>
+        </div> 
+    
+
+    <div className="main10">
+        <div className="headingsec1">
+            <h2 className="headingsec1txt">Accessories</h2>
+        </div>
+       
+        <div className="buttons2" id="shopbtn">
+            <div className="orderbtn2">
+                <a href="#" className="ordertxt2"  >Shop Now</a>
+            </div>
+            
+            
+        </div> 
+    </div>
+    </>
+  )
+}
+
+export default App
